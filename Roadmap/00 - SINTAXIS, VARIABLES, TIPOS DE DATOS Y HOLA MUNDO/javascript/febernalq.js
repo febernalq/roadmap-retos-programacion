7afde131b1611fd0d28014779998138c1c7a4b61
febernalq <141196251+febernalq@users.noreply.github.com>
2024-06-17 18:18:41 -0500

    git .add

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/febernalq.js
@@ -0,0 +1,25 @@
+//https://www.javascript.com//
+
+/* asi se hacen comentarios
+en varias lineas
+y no afecta el codigo
+*/
+
+var myvariable = "mi variable"
+
+const myconstante = "true"
+
+
+// tipos de variables acontinuacion//
+
+let string = "felipe"
+let entero = "33"
+let float = "21.5"
+let boulean = "true"
+let undefined  // esta variable no se a definido//
+let bigInt = "4654564654654654565456"
+let Symbol = "id"
+
+
+console.log (hola,javascript);
+
